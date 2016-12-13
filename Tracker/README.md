c8f27930fc5f8f3c69cd66b63823f8f970f13e36
koningdde <koningdde@gmail.com>
2016-12-13 18:42:52 +0100

    Drawings

---
+++ Tracker/README.md
@@ -0,0 +1,1 @@
+ArduGPS files
---
+++ Tracker/config.h
@@ -0,0 +1,17 @@
+
+// Define your home lat & lon below
+// http://andrew.hedges.name/experiments/convert_lat_long/
+// Goto aprs.fi and find your deg mm.mm ( leave the secs blank )
+// home
+const float HOME_LAT = 50.49;
+const float HOME_LON = 04.45;
+//
+//
+// Custom Comments here
+#define COMMENT "Zet hier eventueel commentaar"
+
+
+
+
+
+
