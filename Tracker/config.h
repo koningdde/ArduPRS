5ee9fc9758bed4b6101e083cb85a055a9e75f7af
Danny de Koning <koningdde@gmail.com>
2016-12-22 18:41:44 +0100

    minor

--- Tracker/config.h
+++ Tracker/config.h
@@ -3,8 +3,8 @@
 // http://andrew.hedges.name/experiments/convert_lat_long/
 // Goto aprs.fi and find your deg mm.mm ( leave the secs blank )
 // home
-const float HOME_LAT = 50.49;
-const float HOME_LON = 04.45;
+//const float HOME_LAT = 51.53;
+//const float HOME_LON = 04.09;
 
 const float HOME_LAT = 51.89235;
 const float HOME_LON = 04.16303;
