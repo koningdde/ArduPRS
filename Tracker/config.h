35be5af04d16a8cea1c2e700bc151e749f2e2d26
Danny de Koning <koningdde@gmail.com>
2016-12-22 18:40:22 +0100

    Test for better base calculation

--- Tracker/config.h
+++ Tracker/config.h
@@ -6,13 +6,16 @@
 const float HOME_LAT = 50.49;
 const float HOME_LON = 04.45;
 
+const float HOME_LAT = 51.89235;
+const float HOME_LON = 04.16303;
+
 // Custom Comments here
-#define COMMENT "Put comments here"
+#define COMMENT "QRV @ 145.475"
 
 // config file to defines callsign, ssid, symbols and home location
-#define MYCALL "NOCALL"
+#define MYCALL "PD1DDK"
 
 #define DUALMODE //Kies indien je 2 verschillende sufix wil gebruiken, nl CALL-9 en CALL-5. Sluit schakelaar aan op pin 3.
 
 // Current Version
-#define VERSION "ArduiPRS v1.2 "
+#define VERSION "ArduiPRS v1.3 "
